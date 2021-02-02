d1721a6e5f7b66af72bcf6b54fa5f892eeedd4bc
Gregor Wolf <gregor.l.wolf@gmail.com>
2021-02-02 05:00:51 +0100

    initial and first home draft

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+//@import './assets/styles/custom.scss';
+@import '../node_modules/bootstrap/scss/bootstrap';
+@import '../node_modules/bootstrap-vue/src/index.scss';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+@font-face {
+  font-family: "WhyteInktrapBold";
+  src: local("WhyteInktrapBold"),   url(./assets/fonts/WhyteInktrap-Bold.ttf) format("truetype");
+}
+
+.inktrap {
+  font-family: WhyteInktrapBold, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <b-container fluid>
+      <h1 class="text-left inktrap display-1">Hi, I'm Gregor!</h1>
+      <h1 class="text-left inktrap display-4">Before I tell you something about me, I'd like to encourage you to make Your own mark on my site.</h1>
+      <h1><b-icon-chevron-double-down/></h1>
+      <b-row v-for="(row, key) in dotmatrix" :key="key" class="mb-4">
+        <span class="mx-auto dot" v-for="(dot) in row" :key="dot.id" @click="dot.on = !dot.on">
+          <b-icon-circle v-if="!dot.on"/>
+          <b-icon-circle-fill v-else/>
+        </span>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  },
+  data(){
+    return {
+      dotmatrix: [],
+    }
+  },
+  created() {
+    for (var i = 0; i < 12; i++) {
+      this.dotmatrix.push([]);
+      for (var j = 0; j < 24; j++) {
+        this.dotmatrix[i].push({id: i+','+j, on:false});
+      }
+    } 
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .dot { transition: transform .2s; }
+  .dot:hover { transform: scale(1.5); }
+</style>
