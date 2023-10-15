b6cf9afaffaef194091d2c29f1fcf09238f0043c
Gregor Wolf <gregor.l.wolf@gmail.com>
2023-10-15 03:43:01 +0200

    add disclaimer

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -4,12 +4,15 @@
     <br>
     <br>
     <h1>Impressum</h1>
+    <p>Ich distanziere mich von den in der Punktematrix auf der Startseite dargestellten Inhalten. <br>
+      Diese sind Ergebnis des kollektiven Interaktionsprozesses und stehen in keinem Zusammenhang zu meinen eigenen Gedanken.
+    </p>
+    <br>
     <p>Angaben gemäß § 5 TMG</p>
     <p><br>Gregor Wolf</p>
     <h4>Postanschrift:</h4>
     <p>Jahnalle 67<br>04177 Leipzig<br></p>
     <h4>Kontakt:</h4>
-    <p></p>E-Mail: hi@gregl.it</p>
-    <p></p><p><br></p>
+    <p></p>E-Mail: hi@gregl.it</p><br>
   </div>
 </template>
